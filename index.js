abd7d59a69d9bb22db58c8f74e801f237a951a43
1noseA <1noseA2L@gmail.com>
2025-08-23 13:24:56 +0900

    タスク削除機能実装

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ const onClickAdd = () => {
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
 
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親にあるliタグを未完了リストから削除
+    // 押したボタンから一番近いliタグを探す
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  })
 
   // liタグの子要素に各要素を設定
   div.appendChild(p);
